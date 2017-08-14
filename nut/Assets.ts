3035b1745ced56aa8caeeea747368a0485ce0b5d
slumpsand <slumpsand@gmail.com>
2017-08-14 20:12:48 +0200

    make the asset stuff fetch assets only once

--- nut/Assets.ts
+++ nut/Assets.ts
@@ -2,90 +2,92 @@ import { Config } from "./Config.js";
 
 export class Assets {
 
-    private imageFiles: { [id: string]: HTMLImageElement };
-    private jsonFiles:  { [id: string]: any };
-    private textFiles:  { [id: string]: string };
-
     private config: Config;
+
+    private imageFiles: { [id: string]: Promise<HTMLImageElement> };
+    private jsonFiles: { [id: string]: Promise<any> };
+    private textFiles: { [id: string]: Promise<string> };
 
     constructor(config: Config) {
         this.imageFiles = {};
         this.jsonFiles = {};
         this.textFiles = {};
+
+        this.config = config;
     }
 
     private checkPath(path: string, hasExtension: boolean = false): boolean {
         if(hasExtension) {
             return path.match(/[a-zA-Z_](?:[a-zA-Z0-9_])+(?:\.[a-z]+)?/).length > 0;
         } else {
-            return path.match(/[a-zA-Z_](:?[a-zA-Z0-9_])+/).length > 0;
+            return path.match(/[a-zA-Z_](?:[a-zA-Z0-9_])+/).length > 0;
         }
     }
 
     getImage(ident: string): Promise<HTMLImageElement> {
-        if(!this.checkPath(ident)) {
+        if (!this.checkPath(ident)) {
             return Promise.reject(`invalid image identifier '${ident}'`);
         }
 
-        if(ident in this.imageFiles) {
-            return Promise.resolve(this.imageFiles[ident]);
+        if(!(ident in this.imageFiles)) {
+            this.fetchImage(ident);
         }
 
-        let path = `data/img/${ident}.png`;
-        console.log(`[Assets.ts]: fetching image file '${path}'`);
+        return this.imageFiles[ident];
+    }
 
-        return fetch(path)
-            .then((resp: Body) => {
-                return resp.blob();
-            }).then((blob: Blob) => {
+    private fetchImage(ident: string): void {
+        let path = `data/img/${ident}.png`;
+        console.log(`[Assets.ts]: fetching image file: '${path}'`);
+
+        this.imageFiles[ident] = fetch(path)
+            .then((resp: Body) => resp.blob())
+            .then((blob: Blob) => {
                 let img = new Image();
                 img.src = URL.createObjectURL(blob);
 
-                this.imageFiles[ident] = img;
                 return img;
             });
     }
 
     getJSON(ident: string): Promise<any> {
-        if(!this.checkPath(ident)) {
+        if (!this.checkPath(ident)) {
             return Promise.reject(`invalid json identifier '${ident}'`);
         }
 
-        if(ident in this.jsonFiles) {
-            return Promise.resolve(this.jsonFiles[ident]);
+        if(!(ident in this.jsonFiles)) {
+            this.fetchJSON(ident);
         }
 
-        let path = `data/json/${ident}.json`;
-        console.log(`[Assets.ts]: fetching json file '${path}'`);
+        return this.jsonFiles[ident];
+    }
 
-        return fetch(path)
-            .then((resp: Body) => {
-                return resp.json();
-            }).then((json: any) => {
-                this.jsonFiles[ident] = json;
-                return json;
-            });
+    private fetchJSON(ident: string): void {
+        let path = `data/json/${ident}.json`;
+        console.log(`[Assets.ts]: fetching json file: '${path}'`);
+
+        this.jsonFiles[ident] = fetch(path)
+            .then((resp: Body) =>  resp.json());
     }
 
     getText(ident: string): Promise<string> {
         if(!this.checkPath(ident, true)) {
-            return Promise.reject(`invalid text identifier '${ident}'`);
+            return Promise.reject(`invalid json identifier '${ident}'`);
         }
 
-        if(ident in this.textFiles) {
-            return Promise.resolve(this.textFiles[ident]);
+        if(!(ident in this.textFiles)) {
+            this.fetchText(ident);
         }
 
+        return this.textFiles[ident];
+    }
+
+    private fetchText(ident: string): void {
         let path = `data/any/${ident}`;
-        console.log(`[Assets.ts]: fetching text file '${path}'`);
+        console.log(`[Assets.ts]: fetching text file: '${path}'`);
 
-        return fetch(path)
-            .then((resp: Body) => {
-                return resp.text();
-            }).then((text: string) => {
-                this.textFiles[ident] = text;
-                return text;
-            });
+        this.textFiles[ident] = fetch(path)
+            .then((resp: Body) => resp.text());
     }
 
 }
